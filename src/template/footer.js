6d49a809f9b1b655d25298638333b3c676298ad9
Jehu <z_zhihao@foxmail.com>
2021-07-05 19:39:19 +0800

    update: footer update

--- src/template/footer.js
+++ src/template/footer.js
@@ -5,7 +5,7 @@ const footer = creator('div', {
   id: 'footer',
   innerHTML: `
     &copy; ${(new Date()).getFullYear()} ${title}. Powered by
-    <a href="https://github.com/LoeiFy/Mirror" target="_blank">Mirror</a> .
+    <a href="https://github.com/wwyx778/Prism" target="_blank">Prism</a> .
     <a href="https://github.com/${user}/${repository}/issues" target="_blank">Source</a>
   `,
 })
